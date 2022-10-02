4b2f5b83866b69f2f8372f5e04c098d3b27b20dc
Cheta Nzeogu <chetanzeogu410@gmail.com>
2022-10-01 20:28:47 -0700

    Update Server

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const http = require('http');
 const server = http.createServer(app)
 const {Server} = require('socket.io');
 const io = new Server(server);
+
+io.on("connection", socket => {
+    console.log("A user is connected")
+});
 app.get('/client/:file',(req,res) => {
     res.sendFile(__dirname + `/client/${req.params.file}`)
 })
@@ -11,6 +15,6 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + "/client/index.html")
 })
 
-server.listen(3001, (err) => {
+server.listen(3000, (err) => {
     console.log("Listening on port 3000")
 })
